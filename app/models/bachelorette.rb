dc6b0f1dcc7cd42b8999c5af0a75d3ea16ee1843
Nathan Trautenberg <Natester123@mac.com>
2023-11-10 12:49:22 -0700

    made model method to find avg age of contestants for a bachelorette.

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -1,4 +1,7 @@
 class Bachelorette < ApplicationRecord
   has_many :contestants
   
+  def avg_age_of_contestants
+    self.contestants.average("age").to_f
+  end
 end
--- spec/models/bachelorette_spec.rb
+++ spec/models/bachelorette_spec.rb
@@ -4,4 +4,13 @@ RSpec.describe Bachelorette, type: :model do
   describe "relationships" do
     it {should have_many :contestants}
   end
+
+  describe '#avg_age_of_contestants' do
+    it 'will calculate avg age of contestants' do
+      test_data
+      expect(@bach1.avg_age_of_contestants).to eq(29)
+      expect(@bach2.avg_age_of_contestants).to eq(21)
+      expect(@bach3.avg_age_of_contestants).to eq(29)
+    end
+  end
 end
